b328d8bfcd24d6ed7b53f352af0914b1b0de24ac
Luke Burns <lukemburns@gmail.com>
2016-05-17 22:25:07 -0400

    separated out proxy experiment into predux

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = obj
 module.exports.ctor = ctor
-module.exports.proxy = proxy
 
 var through2 = require("through2")
 
@@ -25,32 +24,7 @@ function obj (options, fn) {
     fn = options
     options = {}
   }
+  options = options || {}
   options.objectMode = true
   return ctor(options, fn)()
-}
-
-function proxy () {
-  return proxify(obj(function (state, chunk) {
-    chunk = chunk.toString()
-    var i = chunk.indexOf(' ')
-    var name = chunk.slice(0, i)
-    var args = JSON.parse(chunk.slice(i+1))
-    args = [state].concat(args)
-    return this.fns[name].apply(this, args)
-  }))
-}
-
-function proxify (target) {
-  var handler = {
-    set(target, name, fn) {
-      target.fns = target.fns || []
-      target.fns[name] = fn;
-      target[name] = function () {
-        target.write(name + ' ' + JSON.stringify(Array.prototype.slice.call(arguments)))
-      }
-      return true
-    }
-  }
-
-  return new Proxy(target, handler)
 }
